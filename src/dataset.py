f47bc5ac1c61eb48aeb3c841c17a12e0f0a9b62e
YashinSergey <yashinsergey125@gmail.com>
2026-04-26 18:15:41 +0300

    RTC-002 fixed RoomTypeDataset getitem method

--- src/dataset.py
+++ src/dataset.py
@@ -10,15 +10,12 @@ class RoomTypeDataset(Dataset):
             csv_path,
             image_root,
             transform=None,
-            image_col='image',
             target_col='result'
     ):
         # путь к папке с изображениями
         self.image_root = image_root
         # transforms, которые будут применяться к каждому изображению
         self.transform = transform
-        # название колонки, где лежит путь или имя изображения
-        self.image_col = image_col
         # название колонки, где лежит класс изображения
         self.target_col = target_col
         # Загружаем CSV-файл с описанием датасета
@@ -31,17 +28,23 @@ class RoomTypeDataset(Dataset):
     def __getitem__(self, idx):
         # Берём одну строку из таблицы по индексу
         row = self.df.iloc[idx]
-        # Получаем путь или имя файла изображения из нужной колонки
-        image_name = row[self.image_col]
+
+        # Берём id изображения и формируем имя файла
+        image_id = row["image_id_ext"]
+        image_name = f"{image_id}.jpg"
+
         # Собираем полный путь к изображению
         image_path = os.path.join(self.image_root, image_name)
-        # Открываем изображение.
-        # convert("RGB") нужен, чтобы все изображения были в одном формате: 3 канала
+
+        # Открываем изображение
         image = Image.open(image_path).convert("RGB")
+
         # Получаем числовой класс изображения
         target = int(row[self.target_col])
+
         # Если transforms переданы, применяем их к изображению
         if self.transform is not None:
             image = self.transform(image)
+
         # Возвращаем изображение и его класс
         return image, target
